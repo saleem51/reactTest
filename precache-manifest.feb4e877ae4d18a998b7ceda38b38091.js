f67a5d526ac1e9a5565d1cd64188a260c2a69245
saleem51 <orinoco@mail.fr>
2020-10-30 16:03:11 +0100

    Updates

---
+++ precache-manifest.feb4e877ae4d18a998b7ceda38b38091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54573e7dc13b6bc7a973df724532a7f",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "e8f69f6d903217d22896",
+    "url": "/reactTest/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "8330da759dec4db50546",
+    "url": "/reactTest/static/css/main.63b2506c.chunk.css"
+  },
+  {
+    "revision": "e8f69f6d903217d22896",
+    "url": "/reactTest/static/js/2.df4fb949.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTest/static/js/2.df4fb949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8330da759dec4db50546",
+    "url": "/reactTest/static/js/main.21c8bfef.chunk.js"
+  },
+  {
+    "revision": "c8d4acb3ee723c8d5437",
+    "url": "/reactTest/static/js/runtime-main.96a80951.js"
+  }
+]);
